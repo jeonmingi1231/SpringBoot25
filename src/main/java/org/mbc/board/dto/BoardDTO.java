48e7125b7124fa69c36f957ebbcd4c0755e49392
220-07\jeonmingi <alsrl7772@gmail.com>
2025-07-21 10:43:29 +0900

    스트링 부트 테스트 구현 완료

---
+++ src/main/java/org/mbc/board/dto/BoardDTO.java
@@ -0,0 +1,34 @@
+package org.mbc.board.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder  // 빌더패턴은 @AllArgsConstructor , @NoArgsConstructor 필수
+@AllArgsConstructor // 모든 필드를 생성자로
+@NoArgsConstructor // 기본 생성자
+
+public class BoardDTO {
+
+    private Long bno;
+
+    @NotEmpty
+    @Size(min = 3, max = 100)
+
+    private String title ;
+
+    @NotEmpty
+    private String content ;
+    @NotEmpty
+    private String writer ;
+
+    private LocalDateTime regDate ;
+
+    private LocalDateTime modDate ;
+}
