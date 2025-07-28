b6f125b48ce006828a086b2871591195134dd73c
220-07\jeonmingi <alsrl7772@gmail.com>
2025-07-28 09:43:57 +0900

    reply + front

--- src/main/java/org/mbc/board/dto/BoardDTO.java
+++ src/main/java/org/mbc/board/dto/BoardDTO.java
@@ -8,27 +8,37 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
 
 @Data
-@Builder  // 빌더패턴은 @AllArgsConstructor , @NoArgsConstructor 필수
-@AllArgsConstructor // 모든 필드를 생성자로
-@NoArgsConstructor // 기본 생성자
-
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BoardDTO {
 
     private Long bno;
 
     @NotEmpty
     @Size(min = 3, max = 100)
-
-    private String title ;
+    private String title;
 
     @NotEmpty
-    private String content ;
+    private String content;
+
     @NotEmpty
-    private String writer ;
+    private String writer;
 
-    private LocalDateTime regDate ;
+    private LocalDateTime regDate;
 
-    private LocalDateTime modDate ;
+    private LocalDateTime modDate;
+
+    // p640 추가
+    private List<String> fileNames; // 첨부파일 목록
+    // 리포지토리에서 처리는 엔티티는
+    // private Set<BoardImage> imageSet = new HashSet<BoardImage>();
+    // dto를 엔티티로 변환하는 ModelMapper를 사용했었는데 다양한 처리를 위해서 custom
+    // -> 서비스 계층에서 처리
+
+
 }
